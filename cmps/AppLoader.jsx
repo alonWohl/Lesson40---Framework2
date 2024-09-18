77259f7944eb7e18a0e3d1456d3377f8869198cf
alonWohl <alonw1010@gmail.com>
2024-09-19 01:04:55 +0300

    Fix file path to loader

--- cmps/AppLoader.jsx
+++ cmps/AppLoader.jsx
@@ -1,3 +1,3 @@
 export function AppLoader() {
-  return <img className='app-loader' src='../assets/img/loader.svg' alt='loader' />
+  return <img className='app-loader' src='/assets/img/loader.svg' alt='loader' />
 }
