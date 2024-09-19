0c79bd1c8f0cdfb48f38f34cc1b7276a98ec64c3
alonWohl <alonw1010@gmail.com>
2024-09-19 12:59:55 +0300

    Add book details features

--- cmps/BookDetails.jsx
+++ cmps/BookDetails.jsx
@@ -1,13 +1,26 @@
 import { bookSevice } from '../services/book.service.js'
+import { getCurrencySymbol } from '../services/util.service.js'
 import { AppLoader } from './AppLoader.jsx'
 
 const { useState, useEffect } = React
 
 export function BookDetails({ bookId, onSelectBookId }) {
   const [book, setBook] = useState(null)
+
   useEffect(() => {
-    bookSevice.get(bookId).then((book) => setBook(book))
+    loadBook()
   }, [])
+
+  function loadBook() {
+    bookSevice
+      .get(bookId)
+      .then((book) => {
+        setBook(book)
+      })
+      .catch((err) => {
+        console.error('Had issues loading book details', err)
+      })
+  }
 
   function pageCountText(count) {
     if (count >= 500) return '- Serious Reading'
@@ -15,7 +28,20 @@ export function BookDetails({ bookId, onSelectBookId }) {
     if (count <= 100) return '- Light Reading'
   }
 
+  function isNew(year) {
+    const currYear = new Date().getFullYear()
+
+    if (currYear - year >= 10) return ' Vintage'
+    return ' New'
+  }
+
+  function priceClassName(price) {
+    if (price > 150) return 'red'
+    if (price < 20) return 'green'
+  }
+
   if (!book) return <AppLoader />
+
   return (
     <section className='book-details'>
       <img src={book.thumbnail} alt='book-image' />
@@ -30,12 +56,18 @@ export function BookDetails({ bookId, onSelectBookId }) {
         <span>Pages: </span>
         {book.pageCount} {pageCountText(book.pageCount)}
       </p>
+      <p className={priceClassName(book.listPrice.amount)}>
+        <span>Price: </span>
+        {book.listPrice.amount}
+        {getCurrencySymbol(book.listPrice.currencyCode)}
+        {book.listPrice.isOnSale && <span className='sale'> SALE !</span>}
+      </p>
       <p>
         <span>By: </span> {book.authors}
       </p>
       <p>
         <span>Published at: </span>
-        {book.publishedDate}
+        {book.publishedDate} {isNew(book.publishedDate)}
       </p>
       <p>
         <span>Desc: </span>
